8053ba24a83ce1e3e5811f6b2baa42217e426b6f
henry-49 <henryswebdevelopment.com>
2026-04-02 13:47:43 +0200

    first commit installed axios and added api Endpoint

---
+++ src/api/reporting.ts
@@ -0,0 +1,12 @@
+import axios, {AxiosResponse} from "axios";
+
+export const loadOrders = () => {
+    return axios.get('http://localhost:8000/api/orders/')
+
+    .then((response: AxiosResponse) => {
+
+        console.log(response.data);
+
+        return response.data;
+    }) 
+}
